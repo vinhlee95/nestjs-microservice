00b2cb9384a544f87390ed58387fa68e99a1d0dd
Vinh Le <lethanhvinh95@gmail.com>
2023-01-05 07:15:51 +0200

    inject Kafka client to AppService

--- apps/api-gateway/src/app/app.service.ts
+++ apps/api-gateway/src/app/app.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import {CreateUserDto} from '@nestjs-microservices/shared/dto'
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to api-gateway!' };
+  constructor(
+    // Inject Kafka client to this class
+    // Then any instance of this class can .emit() messages to Kafka queue
+    @Inject('AUTH_MICROSERVICE') private readonly client: ClientKafka
+  ) {}
+
+  createUser(payload: CreateUserDto) {
+    this.client.emit('create-user', JSON.stringify(payload));
   }
 }
