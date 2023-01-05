e2a89fd6365ae8e8a2b5085073bcad3e774c7650
Vinh Le <lethanhvinh95@gmail.com>
2023-01-05 07:32:22 +0200

    add User service methods

--- apps/auth-microservice/src/app/app.service.ts
+++ apps/auth-microservice/src/app/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import { CreateUserDto } from '@nestjs-microservices/shared/dto';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to auth-microservice!' };
+  constructor(
+    private readonly usersRepository: UsersRepository
+  ) {}
+
+  createUser(payload: CreateUserDto) {
+    return this.usersRepository.createUser({...payload, id: randomUUID()})
+  }
+
+  getUserById(id: string) {
+    return this.usersRepository.findById(id)
   }
 }
--- apps/auth-microservice/src/app/users.repository.ts
+++ apps/auth-microservice/src/app/users.repository.ts
@@ -6,7 +6,12 @@ import { User } from "@nestjs-microservices/shared/entities";
 export class UsersRepository {
   private readonly users: User[] = []
 
-  createUser(user: User) {
+  createUser(user: User): User {
     this.users.push(user)
+    return user
+  }
+
+  findById(id: string): User {
+    return this.users.find(user => user.id === id)
   }
 }
