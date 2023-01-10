ec4a092a11a3c4859917cb83cf5eb336ed6a2f15
Vinh Le <lethanhvinh95@gmail.com>
2023-01-10 06:21:12 +0200

    init payments module

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PaymentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
